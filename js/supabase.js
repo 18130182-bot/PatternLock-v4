d29e6663a0223ef3a04b6aeabe7d5dddd7f3c4ff
Kushikino School <18130182@kago.ed.jp>
2026-07-10 12:33:02 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 console.log("supabase.js が読み込まれました");
 
 const SUPABASE_URL = "https://kcncleosyltuhuaamque.supabase.co";
-const SUPABASE_ANON_KEY = "あなたの Publishable Key";
+const SUPABASE_ANON_KEY = "sb_publishable_hRCu-BxhlUPQcwr0KB3q_Q_00dIKXd1";
 
 window.db = window.supabase.createClient(
     SUPABASE_URL,
