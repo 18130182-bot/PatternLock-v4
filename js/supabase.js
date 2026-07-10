cf836bbf75540cd97f6ac9e781aece547ca3281c
Kushikino School <18130182@kago.ed.jp>
2026-07-10 14:45:31 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,21 +1,29 @@
-console.log("supabase.js が読み込まれました");
+// ======================================
+// PatternLock-v4
+// Supabase接続
+// ======================================
 
 const SUPABASE_URL = "https://kcncleosyltuhuaamque.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_hRCu-BxhlUPQcwr0KB3q_Q_00dIKXd1";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_hRCu-BxhlUPQcwr0KB3q_Q_00dIKXd1";
 
 window.db = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_PUBLISHABLE_KEY
 );
 
+// 接続テスト
 (async () => {
+
+    console.log("Supabase 接続テスト開始");
+
     const { data, error } = await window.db
         .from("users")
-        .select("*");
+        .select("username");
 
     if (error) {
         console.error("Supabase接続エラー", error);
     } else {
         console.log("Supabase接続成功", data);
     }
+
 })();
