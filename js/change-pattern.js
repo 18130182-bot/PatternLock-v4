026cf1d2506cd33efc186fd3acff9487591037c1
Kushikino School <18130182@kago.ed.jp>
2026-07-13 14:52:03 +0900

    Create change-pattern.js

---
+++ js/change-pattern.js
@@ -0,0 +1,39 @@
+// ======================================
+// PatternLock-v4
+// change-pattern.js
+// ======================================
+
+const saveButton = document.getElementById("saveButton");
+const message = document.getElementById("message");
+
+saveButton.addEventListener("click", async () => {
+
+    if (pattern.length < 4) {
+        message.style.color = "#dc2626";
+        message.textContent = "4点以上選択してください";
+        return;
+    }
+
+    const patternString = pattern.join("-");
+
+    const { error } = await window.db
+        .from("users")
+        .update({
+            pattern_hash: patternString
+        })
+        .eq("username", "admin");
+
+    if (error) {
+        console.error(error);
+
+        message.style.color = "#dc2626";
+        message.textContent = "保存に失敗しました";
+        return;
+    }
+
+    message.style.color = "#16a34a";
+    message.textContent = "保存しました！";
+
+    clearPattern();
+
+});
